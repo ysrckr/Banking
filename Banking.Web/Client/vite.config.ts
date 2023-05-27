9989f616a8eb585036dfd45e6e892a9f87a57e83
Yasar Cakir <yasar.cakir@mirsis.com.tr>
2023-05-27 14:39:38 +0300

    Initial commit

---
+++ Banking.Web/Client/vite.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import mkcert from 'vite-plugin-mkcert'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    base: '/',
+    server: {
+        https: true,
+        port: 6363
+    },
+    plugins: [react(), mkcert()],
+})
